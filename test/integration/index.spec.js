8c1fd91790b38002c650dbf791ebaa1e21e2796f
GProst <gprostmail@gmail.com>
2017-03-24 21:03:40 +0300

    integration tests 100% coverage

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -1,10 +1,18 @@
+const expect = require('chai').expect;
 const webpack2 = require('webpack');
 const config = require('./webpack.config');
+const path = require('path');
+const fs = require('fs');
 
 describe('webpack-clean-obsolete-chunks plugin', () => {
   describe('in webpack2 watch mode', () => {
-    it(`SHOULD remove all obsolete files`, () => {
+    it(`SHOULD remove all obsolete (only) files`, (done) => {
       const compiler = webpack2(config);
+      let firstCompilation = true;
+      let oldFiles;
+      let newFiles;
+      let fileToChange = path.join(__dirname, 'test-entry-files/app/partB.js');
+      let changedFileInitialContent;
       
       const watching = compiler.watch({
         aggregateTimeout: 300,
@@ -12,8 +20,24 @@ describe('webpack-clean-obsolete-chunks plugin', () => {
       }, (err, stats) => {
         handleErrors(err, stats);
         
-        //TODO: saving and checking files
-        
+        if (firstCompilation) {
+          oldFiles = fs.readdirSync(path.join(__dirname, 'test-output-files'));
+          changedFileInitialContent = fs.readFileSync(fileToChange, 'utf-8');
+          //following should only change app.*.js and app.*.js.map
+          fs.writeFileSync(fileToChange, 'testString');
+          firstCompilation = false;
+        } else {
+          //restore initial file content
+          fs.writeFileSync(fileToChange, changedFileInitialContent);
+          
+          newFiles = fs.readdirSync(path.join(__dirname, 'test-output-files'));
+          let changedFiles = oldFiles.filter(oldFile => newFiles.indexOf(oldFile) === -1);
+          expect(changedFiles).to.have.length.above(0);
+          changedFiles.forEach(changedFile => {
+            expect(changedFile).to.match(/app.*.js.*/);
+          });
+          done();
+        }
       });
       
     });
